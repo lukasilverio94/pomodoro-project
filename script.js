49e5f3d58e7f46661c6b18fa5c75a66696607a42
lukasilverio94 <lukasilverio94@gmail.com>
2023-11-05 00:28:55 +0100

    break and section can now increase/decrease

--- script.js
+++ script.js
@@ -5,26 +5,25 @@ const startBtn = document.getElementById("start-btn");
 const pauseBtn = document.getElementById("pause-btn");
 const resetBtn = document.getElementById("reset-btn");
 //Reset Container Elements
-const increaseBreak = document.getElementById("increase-break");
+const increaseBreak = document.getElementById("increase-break-btn");
 const breakTime = document.getElementById("break-time");
-const decreaseBreak = document.getElementById("decrease-break");
+const decreaseBreak = document.getElementById("decrease-break-btn");
 //Edit Pomodoro Section Time
 const increaseSectionBtn = document.getElementById("increase-section");
 const decreaseSectionBtn = document.getElementById("decrease-section");
+const sectionTimeDisplay = document.getElementById("section-time");
 
 let startingMinutes = 25;
-let startBreak = 2;
+let startBreak = 5;
 let time = startingMinutes * 60;
 let timer; // to store the interval timer
 let inBreak = false;
-
-mainSectionText.innerText = "Session";
+// mainSectionText.innerText = "Session";
 //UpdatedCountDown()
 function updatedCountDown() {
   const minutes = Math.floor(time / 60);
   let seconds = time % 60;
   countDown.innerText = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
-
   if (time <= 0) {
     stop();
     countDown.innerText = `${breakTime.innerText}:00`;
@@ -34,7 +33,6 @@ function updatedCountDown() {
     time--;
   }
 }
-
 //Start Countdown Event
 function startTimer() {
   timer = setInterval(() => {
@@ -43,7 +41,6 @@ function startTimer() {
   }, 100);
   toggleBtn();
 }
-
 //Start Break
 function startBreakFun() {
   if (inBreak) {
@@ -63,7 +60,6 @@ function startBreakFun() {
   updatedCountDown(); // Update the countdown display immediately
   startTimer();
 }
-
 //Clear Interval
 function stop() {
   clearInterval(timer);
@@ -76,11 +72,16 @@ function pauseTimer() {
 
 //Reset State
 function resetTimer() {
-  stop(); // Stop the timer
+  stop();
+  startingMinutes = 25;
+  startBreak = 5;
+  sectionTimeDisplay.innerText = "5";
   time = startingMinutes * 60; // Reset time to the initial session time (25 minutes)
   mainSectionText.innerText = "Session"; // Set the text back to "Session"
   countDown.innerText = `${startingMinutes}:00`; // Display the initial time
-  updatedCountDown()
+  breakTime.innerText = startBreak;
+  sectionTimeDisplay.innerText = startingMinutes;
+  updatedCountDown();
   toggleBtn();
 }
 //Toggle buttons classes
@@ -97,6 +98,35 @@ startBtn.addEventListener("click", () => {
   } else {
     startTimer();
   }
+});
+
+//Increase Pomodoro Time
+increaseSectionBtn.addEventListener("click", () => {
+  startingMinutes += 1;
+  time = startingMinutes * 60;
+  countDown.innerText = `${startingMinutes}:00`;
+  sectionTimeDisplay.innerText = startingMinutes;
+});
+
+//Decrease pomodoro time
+decreaseSectionBtn.addEventListener("click", () => {
+  startingMinutes -= 1;
+  time = startingMinutes * 60;
+  countDown.innerText = `${startingMinutes}:00`;
+  sectionTimeDisplay.innerText = startingMinutes;
+});
+
+//Increase Break Time
+increaseBreak.addEventListener("click", () => {
+  startBreak += 1;
+  time = startBreak;
+  breakTime.innerText = startBreak;
+});
+//Decrease Break Time
+decreaseBreak.addEventListener("click", () => {
+  startBreak -= 1;
+  time = startBreak;
+  breakTime.innerText = startBreak;
 });
 pauseBtn.addEventListener("click", pauseTimer);
 resetBtn.addEventListener("click", resetTimer);
